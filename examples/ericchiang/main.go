4db812aa48d697da38d5338586493472f7db5c97
Ewout Prangsma <ewout@prangsma.net>
2018-03-18 12:13:38 +0100

    Added support for `github.com/ericchiang/k8s` library

--- examples/ericchiang/main.go
+++ examples/ericchiang/main.go
@@ -5,14 +5,16 @@ import (
 	"log"
 	"time"
 
-	http "github.com/YakLabs/k8s-client/http"
+	kc "github.com/ericchiang/k8s"
 	lock "github.com/pulcy/kube-lock"
-	k8s "github.com/pulcy/kube-lock/k8s"
+	k8s "github.com/pulcy/kube-lock/k8s/ericchiang"
 )
 
 var (
 	args struct {
 		namespace      string
+		daemonSetName  string
+		deploymentName string
 		replicaSetName string
 		serviceName    string
 	}
@@ -20,6 +22,8 @@ var (
 
 func init() {
 	flag.StringVar(&args.namespace, "namespace", "", "Kubernetes namespace of resource")
+	flag.StringVar(&args.daemonSetName, "daemonSet", "", "Kubernetes namespace of DaemonSet to store lock data in")
+	flag.StringVar(&args.deploymentName, "deployment", "", "Kubernetes namespace of Deployment to store lock data in")
 	flag.StringVar(&args.replicaSetName, "replicaSet", "", "Kubernetes namespace of ReplicaSet to store lock data in")
 	flag.StringVar(&args.serviceName, "service", "", "Kubernetes namespace of Service to store lock data in")
 }
@@ -30,23 +34,24 @@ func main() {
 	if args.namespace == "" {
 		log.Fatalln("-namespace not set")
 	}
-	if args.replicaSetName == "" && args.serviceName == "" {
-		log.Fatalln("-replicaSet or -service must be set")
-	}
 
-	c, err := http.NewInCluster()
+	c, err := kc.NewInClusterClient()
 	if err != nil {
 		log.Fatalf("Cannot create k8s client: %#v\n", err)
 	}
 
 	ttl := time.Second * 30
 	var l lock.KubeLock
-	if args.serviceName != "" {
+	if args.daemonSetName != "" {
+		l, err = k8s.NewDaemonSetLock(args.namespace, args.daemonSetName, c, "", "", ttl)
+	} else if args.deploymentName != "" {
+		l, err = k8s.NewDeploymentLock(args.namespace, args.deploymentName, c, "", "", ttl)
+	} else if args.serviceName != "" {
 		l, err = k8s.NewServiceLock(args.namespace, args.serviceName, c, "", "", ttl)
 	} else if args.replicaSetName != "" {
 		l, err = k8s.NewReplicaSetLock(args.namespace, args.replicaSetName, c, "", "", ttl)
 	} else {
-		log.Fatalln("Unknown resource")
+		l, err = k8s.NewNamespaceLock(args.namespace, c, "", "", ttl)
 	}
 
 	for {
